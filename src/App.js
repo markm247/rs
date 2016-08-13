d48623cc120401c51e1a7956c95c0f182d8b59c1
markm247 <mark.meadows@gmail.com>
2016-08-13 07:55:45 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1 className="App-header-title">Mark Meadows</h1>
+        </div>
+        <p className="App-intro">
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
